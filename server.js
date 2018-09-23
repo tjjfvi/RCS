b0382ab8891ce5ac318d6d8fec666a5eeea2f89a
tjjfvi <tjjfvi@tjjfvi.com>
2018-09-23 18:21:54 -0400

    fixed bug in server.js

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ const fs = require("fs-extra");
 router.use(express.static(__dirname + "/static"));
 
 router.get("/js/preloadDependencies.js", async (req, res) => {
-	let paths = await globby([
-		"static/js/**",
-		"!static/js/index.js",
-	]);
+	let paths = await fs.readdir(__dirname + "/static/js/");
+
+	console.log(paths);
 
 	paths.sort();
 
-	let slicedPaths = paths.map(p => p.slice(10, -3));
+	let slicedPaths = paths.map(p => p.slice(0, -3));
 
 	let properties = slicedPaths.map(p => `\t${JSON.stringify(p)}: require(${JSON.stringify("./" + p)}),`);
 
